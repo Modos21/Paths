00e93afebb543e81e5b1c7125963a226c6a2ddae
Modos <mattes.kania@gmail.com>
2024-06-30 09:34:56 +0200

    Changed Blessing field to custom Functional Interface "Effect"

--- src/main/java/me/modos21/paths/paths/blessings/Blessing.java
+++ src/main/java/me/modos21/paths/paths/blessings/Blessing.java
@@ -1,9 +1,6 @@
 package me.modos21.paths.paths.blessings;
 
-import me.modos21.paths.entities.PathPlayer;
 import me.modos21.paths.paths.Path;
 
-import java.util.function.Consumer;
-
-public record Blessing(Path path, String name, Consumer<PathPlayer> effect) {
+public record Blessing(Path path, String name, Effect effect) {
 }
